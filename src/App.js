04fd093c5b09bea8493beadb2cacb99bb7769ce3
testexp <48822590+testexp@users.noreply.github.com>
2019-04-02 20:41:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ModuleA() {
   return (
     <div>
-      Hello from Module A PR 3 should render
+      Hello from Module A PR 3 should render prdeploy
     </div>
   );
 }
